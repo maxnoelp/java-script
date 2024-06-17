900488ace6e10ed7b9e590a133f15973bdc8ea36
Max Noelp <noelpmax@gmail.com>
2024-06-17 16:06:59 +0200

    Coding Task: First Function

---
+++ Coding-task-first-function/script.js
@@ -0,0 +1,36 @@
+// TODO: implement welcomeMsg function
+
+console.log(welcomeMsg("Jane"));
+
+// Welcome Jane!
+
+console.log(welcomeMsg("Marc"));
+// Welcome Marc!
+
+function welcomeMsg() {
+  console.log("Welcome Jane");
+  console.log("Welcome Marc");
+}
+
+// TODO: Implement the addPositive function
+
+console.log(addPositive1(2, 3));
+// result1 should be 5
+
+function addPositive1() {
+  return 2 + 3;
+}
+
+console.log(addPositive2(3, -5));
+// result2 should be 8
+
+function addPositive2() {
+  return 3 + 5;
+}
+
+console.log(addPositive(-1, -8));
+// result2 should be 9
+
+function addPositive() {
+  return 1 + 8;
+}
