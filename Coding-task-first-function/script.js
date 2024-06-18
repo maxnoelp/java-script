cd14f8bbd9e14ada5c869965daeaefc6748223c1
Max Noelp <noelpmax@gmail.com>
2024-06-18 09:16:53 +0200

    Math.abs

--- Coding-task-first-function/script.js
+++ Coding-task-first-function/script.js
@@ -18,19 +18,15 @@ console.log(addPositive1(2, 3));
 // result1 should be 5
 
 function addPositive1() {
-  return 2 + 3;
+  return Math.abs(2 + 3);
 }
 
-console.log(addPositive2(3, -5));
+console.log(addPositive(3, -5));
 // result2 should be 8
 
-function addPositive2() {
-  return 3 + 5;
+function addPositive() {
+  return Math.abs(3 + 5);
 }
 
 console.log(addPositive(-1, -8));
 // result2 should be 9
-
-function addPositive() {
-  return 1 + 8;
-}
